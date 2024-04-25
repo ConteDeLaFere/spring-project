cb2b7d94a1ae4568816cd473349e367d92201bee
Nikita Malinkin <nik.malina20@gmail.com>
2024-04-25 20:00:55 +0300

    Initial commit

---
+++ src/main/java/com/malina/springtest/myhomework/MyHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.malina.springtest.myhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHomeworkApplication.class, args);
+	}
+
+}
